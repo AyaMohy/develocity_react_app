cd2158001ed9ad704c6b61c02cbef5cce34ef657
AyaMohy <aya.mohey162@gmail.com>
2022-06-23 10:46:47 +0200

    adding bootstrap package

---
+++ src/components/LeftHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function LeftHeader(){
+    return (
+        <>
+            <section>
+                <h5 className="">BETA STAGE</h5>
+                <h2>
+                    Became a PRO in Crypto Investments
+                </h2>
+            </section>
+        </>
+    )
+}
