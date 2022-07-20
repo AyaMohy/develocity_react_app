c56e084d47ccab9271cda719d20269726ca54549
ahmhama <ahmhama61@gmail.com>
2022-07-20 19:08:09 +0200

    aaa

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import { NavBar } from '../../components/Home/Header/NavBar'
+import { Section1 } from '../../components/Home/Main/section1/Section1'
+import { Section2 } from '../../components/Home/Main/section2/Section2'
+import CopyRight from '../../components/Home/CopyRight/CopyRight'
+
+function Home() {
+ 
+
+  return (
+    <div >
+      <h1>يامسهل يارب</h1>
+      <NavBar />
+      <Section1 />
+      <Section2 />
+      <CopyRight />
+    </div>
+  );
+}
+
+export { Home };
+
